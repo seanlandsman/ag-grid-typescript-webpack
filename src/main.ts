4099a17d63f37ac75edc625611b40dc6c3fb3b77
Sean Landsman <sean@thelandsmans.com>
2019-03-01 13:55:16 +0000

    AG-1329 Add Modularisation to reduce grid bundle size - SPIKE

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-// with    5741085 Feb 27 17:42 main.bundle.js
-// without 5715900 Feb 27 17:43 main.bundle.js
-// 25k difference
-
 // 1) All "exposed" items should have interfaces - these can then be exposed/used as necessary to retain type safety but without
 // us referencing the actual entity (which would mean webpack would include it)
 
 // 2) References in Bean.ts need to be removed and references to any "dynamic" beans be replaced with
 // this.beans.context.getBean('tooltipManager').registerTooltip(this);
+
+// 3) CI will be important to ensure module code isn't accidentally referenced and therefore implicitly included in any bundle
+
+// 4) Anything explosed in main.d.ts/js should be an interface if part of a module (see 3)
 
 import {Grid} from "ag-grid-community";
 
